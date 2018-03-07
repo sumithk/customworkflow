4a6a49f3208d4bec1fcc09b1feebe99855bcfc06
nikhilkesari <nikhil.kesari@bigbasket.com>
2018-03-07 10:40:06 +0530

    first checkin

---
+++ webpack.config.dev.js
@@ -0,0 +1,24 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  devtool: 'cheap-module-eval-source-map',
+  target: 'web',
+  mode: "development",
+  entry: [
+    'webpack-hot-middleware/client?reload=true',
+    path.resolve(__dirname, 'src/index.js')
+  ],
+  output: {
+    path: path.resolve(__dirname, 'src'),
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  module: {
+    rules: [
+      {test: /\.js$/, exclude: /node_modules/, use: [{ "loader":"babel-loader"}, {"loader":"eslint-loader"}]}
+    ]
+  }
+};
